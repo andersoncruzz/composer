bb2fe1fab6a0d05aec25fa083c711508984e2c91
bob <ericabertan@gmail.com>
2016-09-02 10:45:44 -0400

    Parser

--- NovoComposer/salvarQuestionario.php
+++ NovoComposer/salvarQuestionario.php
@@ -1,10 +1,13 @@
 <meta charset="utf-8">
 <?php
 	$arquivo = fopen("./tmp/objeto.json","r+") or die("Unable to open html file!");
+	$var = $arquivo;
 	fseek($arquivo,0);
+	fwrite($arquivo, "");
 	fwrite($arquivo,"data='[");
+	fwrite($arquivo, $var)
 	fseek($arquivo, 0, SEEK_END);
-	fwrite($arquivo, "]';");
+	fwrite($arquivo, "]'");
 	str_replace("}{", "},{", $arquivo);
 	print "Questionário salvo em ./templates";
 	fclose($arquivo);
