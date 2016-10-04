f7664cad67acc005a576768f3216c5a6d17ed587
Larissa Bentes <lgpbentes@gmail.com>
2016-10-04 17:28:59 -0400

    Exclusão do ObjGaleria do Capítulo

--- NovoComposer/composer/frontend/views/objgaleria/view.php
+++ NovoComposer/composer/frontend/views/objgaleria/view.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Atualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Excluir', ['delete', 'id' => $model->id], [
+        <?= Html::a('Excluir', ['delete', 'id' => $model->id, 'capitulo_id'=> $capitulo_id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Tem certeza que deseja excluir esse item?',
